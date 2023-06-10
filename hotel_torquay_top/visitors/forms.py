6562b110c6c8fa10ed79a974154ba1cbe5389d5b
imkozin <ivan.kozin@hotmail.com>
2023-06-11 00:24:54 +0300

    price not displayed

--- hotel_torquay_top/visitors/forms.py
+++ hotel_torquay_top/visitors/forms.py
@@ -1,4 +1,8 @@
+from typing import Any, Dict, Mapping, Optional, Type, Union
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from .models import Guest, Room, Booking, RoomType, RoomSize, RoomRate, UserRequest
 from django.utils import timezone
 
@@ -20,6 +24,10 @@ class BookingForm(forms.ModelForm):
             'check_in' : forms.DateInput(attrs={'type' : 'date'}),
             'check_out' : forms.DateInput(attrs={'type' : 'date'})
         }
+    
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['room'].queryset = Room.objects.filter(is_available=True)
 
     def clean(self):
         cleaned_data = super().clean()
